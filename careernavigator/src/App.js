b2c003004057ad15231749ab3cff419504f7695c
anirudh0224 <anisakhat4@gmail.com>
2024-12-24 17:26:08 +0530

    Create App.js

---
+++ careernavigator/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Home from './Components/Home';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <>
+      <Home></Home>
+    </>
+  );
+}
+
+export default App;
