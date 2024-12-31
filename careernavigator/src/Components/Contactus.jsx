64da1aa38e39d61c78e9e5c85ec82001843790ae
anirudh0224 <anisakhat4@gmail.com>
2024-12-31 08:49:50 +0530

    Create Contactus.jsx

---
+++ careernavigator/src/Components/Contactus.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../Components/css/Contactus.css';
+
+function Contactus() {
+  return (
+    <div>
+      <div className='mainBg'>
+            <h1>Hello</h1>
+      </div>
+    </div>
+  )
+}
+
+export default Contactus
