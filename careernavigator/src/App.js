90a56c3185b5ff8a0133e431985dddf65af6ed7f
anirudh0224 <anisakhat4@gmail.com>
2024-12-31 08:49:48 +0530

    Update App.js

--- careernavigator/src/App.js
+++ careernavigator/src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import Home from './Components/Home';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Contactus from './Components/Contactus';
 
 function App() {
   return (
     <>
-      <Home></Home>
+      {/* <Home></Home> */}
+      <Contactus></Contactus>
     </>
   );
 }
