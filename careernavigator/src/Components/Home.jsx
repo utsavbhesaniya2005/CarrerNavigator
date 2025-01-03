5a8d63468f4a32b60a55e941c480598f0bf6b3b1
anirudh0224 <anisakhat4@gmail.com>
2025-01-03 22:41:24 +0530

    anirudh

--- careernavigator/src/Components/Home.jsx
+++ careernavigator/src/Components/Home.jsx
@@ -1,9 +1,81 @@
-import React from 'react';
+import React,{ useState, useEffect } from 'react';
 import { Navbar, Nav, Container, Button, Row, Col } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/Home.css';
 import video from './Video/home1.mp4';
+
+
+
+
+const PercentageCol = ({ iconClass, title, targetPercentage, description }) => {
+    const [percentage, setPercentage] = useState(0);
+    useEffect(() => {
+        let currentPercentage = 0;
+        const interval = setInterval(() => {
+          currentPercentage += 1;
+          if (currentPercentage >= targetPercentage) {
+            clearInterval(interval);
+            currentPercentage = targetPercentage;
+          }
+          setPercentage(currentPercentage);
+        }, 100);
+    
+        return () => clearInterval(interval); 
+      }, [targetPercentage]);
+      return (
+        <Col lg={3} md={6} sm={12} xm={12} style={{ marginTop: "10vh", padding: "5px" }}>
+          <div
+            style={{
+              borderBottom: "1px solid rgba(103, 98, 98, 0.4)",
+              paddingBottom: "15px",
+              marginBottom: "5vh",
+              width: "98%",
+              display: "flex",
+              alignItems: "center",
+            //   justifyContent:"space-evenly",
+            }}
+          >
+            <div className="icon-1 flex" style={{ color: "white", fontSize: "20px" }}>
+              <i className={iconClass}></i>
+            </div>
+            <span style={{ marginLeft: "7px", fontWeight: "500", fontSize: "20px" }}>{title}</span>
+          </div>
+          <div>
+            <h1 className="percentage-achive">{percentage}%</h1>
+            <span style={{ fontSize: "18px", color: "rgb(130, 126, 126)" }}>{description}</span>
+          </div>
+        </Col>
+      );
+}
+
 const Home = () => {
+    const data = [
+        {
+          iconClass: "fa-solid fa-briefcase",
+          title: "Compensation & Benefits",
+          targetPercentage: 65,
+          description: "Metus torquent platea aenean orci ridiculus potenti feugiat ultricies.",
+        },
+        {
+          iconClass: "fa-solid fa-people-group",
+          title: "Training & Development",
+          targetPercentage: 90,
+          description: "Risus auctor platea vulputate volutpat ultricies, primis fermentum himenaeos.",
+        },
+        {
+          iconClass: "fa-solid fa-medal",
+          title: "Recruitment & Staffing",
+          targetPercentage: 72,
+          description: "Tellus lacus conubia donec curabitur class nulla lorem torquent.",
+        },
+        {
+          iconClass: "fa-solid fa-person-circle-check",
+          title: "Employee Relations",
+          targetPercentage: 58,
+          description: "Ornare tortor vitae litora velit ipsum eleifend dis eget placerat nullam.",
+        },
+      ];
+    
     return (
         <div style={{ backgroundColor: "#D9F1FF" }}>
             {/* Main Content */}
@@ -55,11 +127,18 @@ const Home = () => {
                     </div>
                 </div>
             </main >
-            <Container style={{margin: "20px auto",padding:"20px 10px",overflow:"hidden"}}>
+            <Container style={{margin: "70px auto",padding:"20px 10px",overflow:"hidden"}}>
                 <div className="emp-expr flex">Empolyee Experience <i class="fa-solid fa-arrow-right" style={{marginLeft:"8px"}}></i></div>
                 <Row className=''>
-                    <Col lg={7} md={7} sm={12} xm={12} >
-                        <h1 style={{textTransform:"capitalize",fontWeight:"700",letterSpacing:"2px"}} className='col-h1'>Strategies to Enhance Engagement and Well-Being</h1>
+                    <Col lg={7} md={7} sm={12} xm={12} className='second-cl'>
+                        <div data-aos="zoom-in"
+                             data-aos-offset="200"
+                             data-aos-duration="1000"
+                             data-aos-easing="ease-in-out"
+                             data-aos-mirror="true"
+                             data-aos-once="false">
+                                <h1 style={{textTransform:"capitalize",fontWeight:"700",letterSpacing:"2px"}} className='col-h1'>Strategies to Enhance Engagement and Well-Being</h1>
+                        </div>
                     </Col>
                     <Col lg={5} md={5} sm={12} xm={12} className='second-cl'>
                         <a type="button" href="#" className="btn1">
@@ -72,9 +151,23 @@ const Home = () => {
                         </a>
                     </Col>
                 </Row>
+                <Row>
+
+                <div style={{ display: "flex", flexWrap: "wrap" }}>
+      {data.map((item, index) => (
+        <PercentageCol
+          key={index}
+          iconClass={item.iconClass}
+          title={item.title}
+          targetPercentage={item.targetPercentage}
+          description={item.description}
+        />
+      ))}
+    </div>
+
+                </Row>
             </Container>
         </div>
     );
 };
-
 export default Home;
